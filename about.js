d01ec45a601860b1ce9c382330562333d859f9cf
Matt Blanco <mblanco1000@gmail.com>
2020-04-18 13:03:05 -0700

    Added Blog Section

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 var test = d3.select("body").append("svg")
-.attr("width", window.innerWidth - 50)
-.attr("height", 600);
+.attr("width", window.innerWidth)
+.attr("height", 750);
 
 // Red Square
 test.append("rect")
@@ -40,7 +40,7 @@ test.append("rect")
 // Light Green Square
 test.append("rect")
 .attr("x", -600)
-.attr("y", 500)
+.attr("y", 320)
 .attr("width", 500)
 .attr("height", 20)
 .attr("fill", "#e2f0cb")
@@ -57,9 +57,9 @@ test.append("rect")
 // Darker Green Square
 test.append("rect")
 .attr("x", window.innerWidth)
-.attr("y", 485)
+.attr("y", 330)
 .attr("width", 500)
-.attr("height", 20)
+.attr("height", 25)
 .attr("fill", "#b5ead7")
 .transition()
 .on("start", function repeat() {
@@ -68,5 +68,40 @@ test.append("rect")
   .transition()
   .duration(10000)
   .attr("x", -500)
+  .on("end", repeat)
+});
+
+
+// Orange Square
+test.append("rect")
+.attr("x", -800)
+.attr("y", 680)
+.attr("width", 300)
+.attr("height", 25)
+.attr("fill", "#ffdac1")
+.transition()
+.on("start", function repeat() {
+  d3.select(this)
+  .attr("x", -800)
+  .transition()
+  .duration(8500)
+  .attr("x", window.innerWidth)
+  .on("end", repeat)
+});
+
+// Light Green Square
+test.append("rect")
+.attr("x", -500)
+.attr("y", 700)
+.attr("width", 500)
+.attr("height", 20)
+.attr("fill", "#e2f0cb")
+.transition()
+.on("start", function repeat() {
+  d3.select(this)
+  .attr("x", -600)
+  .transition()
+  .duration(9000)
+  .attr("x", window.innerWidth)
   .on("end", repeat)
 });
