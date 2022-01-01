de643f1c28a57d0b3ec1d6ce40615c73def81b47
mattb311 <blanco.m@husky.neu.edu>
2022-01-01 15:55:21 -0500

    comparing design language project

---
+++ link_resources/CSAndDesignVis/script.js
@@ -0,0 +1,224 @@
+let uistCsv = `Search_Term,Word,nOne,nThree,nFive,nTen,nFifteen,nTwenty,nTwentyFive,nFifty,wordOccurrence
+design,space,99,0,2,3,1,1,1,0,544
+design,tool,51,2,2,3,1,2,3,4,644
+design,process,49,0,2,4,3,2,1,3,603
+design,goals,27,0,1,0,1,2,0,0,77
+design,guidelines,24,3,1,0,1,1,0,0,78
+design,tools,24,2,0,3,6,3,4,1,670
+design,parameters,22,2,1,0,1,2,2,1,416
+design,drawing,20,0,0,0,1,1,2,1,189
+design,considerations,16,1,0,0,0,0,0,0,44
+design,implications,16,0,0,2,1,0,0,0,63
+design,vibrotactile,12,4,2,4,4,5,1,3,231
+design,principles,12,0,2,0,0,0,0,0,67
+design,survey,12,0,0,0,2,0,1,0,206
+design,system,9,10,9,9,7,7,2,9,1741
+design,goal,9,1,3,0,0,0,0,0,192
+design,applications,8,3,1,1,0,4,0,2,720
+design,patterns,8,2,1,0,0,1,1,0,229
+design,choices,8,0,0,0,0,0,0,0,37
+design,elements,7,0,0,1,0,0,1,4,503
+design,principle,6,0,1,1,0,0,0,0,72
+design,study,5,13,7,10,7,5,6,4,1653
+design,virtual,5,3,6,6,3,5,1,7,1539
+design,based,5,2,1,3,1,1,5,1,910
+design,material,5,1,3,0,2,1,0,1,422
+design,session,5,0,0,1,0,2,1,0,168
+design,heuristics,5,0,0,1,0,1,0,1,60
+design,drawings,5,0,0,0,0,1,1,0,21
+design,practice,5,0,0,0,0,0,0,2,153
+design,iteration,5,0,0,0,0,0,0,0,33
+design,decisions,5,0,0,0,0,0,0,0,41
+design,criteria,5,0,0,0,0,0,0,0,48
+design,example,4,8,2,0,1,2,2,3,1145
+design,implementation,4,2,2,0,1,0,1,1,267
+design,methods,4,2,1,1,1,2,1,0,536
+design,approach,4,2,0,1,3,0,0,2,829
+design,features,4,1,0,2,1,0,0,1,419
+design,files,4,1,0,0,0,2,0,0,220
+design,prototyping,4,0,2,2,1,0,3,1,212
+design,intent,4,0,2,0,1,0,0,0,106
+design,requirements,4,0,1,2,0,1,0,0,141
+design,challenge,4,0,1,0,1,0,0,1,149
+design,guideline,4,0,1,0,0,0,0,0,14
+design,automation,4,0,0,1,0,0,1,0,142
+design,thinking,4,0,0,0,1,0,0,1,26
+design,morgan,4,0,0,0,0,0,0,0,22
+design,users,3,6,7,5,6,7,5,2,2391
+design,work,3,5,3,4,2,3,6,4,1375
+design,techniques,3,2,4,1,2,1,0,1,462
+design,insights,3,2,2,0,0,1,0,0,79
+design,tactile,3,2,2,0,0,0,1,3,562`
+
+let drsCsv = `Search_Term,Word,nOne,nThree,nFive,nTen,nFifteen,nTwenty,nTwentyFive,nFifty,wordOccurrence
+design,process,506,42,35,37,45,45,32,34,2128
+design,education,306,27,35,19,30,28,26,19,978
+design,thinking,292,19,18,22,20,24,12,19,940
+design,studies,204,48,22,25,22,16,23,16,784
+design,practice,166,46,18,19,17,23,34,23,986
+design,processes,103,15,10,14,11,11,13,15,625
+design,methods,98,17,27,13,13,13,13,20,800
+design,dna,92,4,4,6,0,6,3,0,123
+design,issues,91,22,13,15,14,14,4,8,506
+design,studio,83,4,8,8,11,7,9,9,357
+design,management,74,18,7,15,11,14,18,16,568
+design,practices,71,15,9,8,10,6,6,9,610
+design,teams,71,3,8,6,7,8,2,7,342
+design,innovation,70,66,23,30,22,27,31,25,872
+design,approach,69,16,20,15,22,13,16,10,973
+design,solutions,68,7,6,1,12,8,9,6,402
+design,activities,67,10,7,9,10,3,8,4,660
+design,students,61,27,18,21,22,21,29,18,1252
+design,approaches,61,8,12,5,14,14,10,10,512
+design,projects,60,9,10,6,16,14,14,8,540
+design,concepts,60,4,19,13,13,13,10,9,445
+design,project,56,13,17,11,20,18,19,16,1044
+design,concept,56,7,8,5,4,13,5,8,459
+design,researchers,54,15,4,1,1,5,5,4,352
+design,tools,53,18,8,17,9,16,16,9,617
+design,team,53,2,19,4,11,13,7,13,649
+design,sprint,53,0,1,2,0,1,4,4,60
+design,principles,52,4,5,5,10,7,5,7,340
+design,sprints,52,1,2,0,0,1,1,0,52
+design,practitioners,46,12,8,5,5,3,5,3,208
+design,outcomes,45,5,4,11,4,3,8,4,426
+design,heuristics,44,1,1,1,3,1,3,0,69
+design,space,43,11,8,8,3,8,7,8,620
+design,activity,42,5,7,2,5,5,4,6,433
+design,history,41,5,5,4,6,5,4,4,234
+design,strategy,38,11,5,8,9,5,7,7,312
+design,theory,34,10,16,12,7,6,7,14,529
+design,disciplines,33,2,2,3,8,5,4,3,249
+design,work,32,17,9,19,18,20,12,14,1321
+design,considerations,32,5,1,5,0,1,3,4,149
+design,strategies,32,3,5,9,5,7,5,7,356
+design,ideas,32,1,4,3,7,12,8,4,565
+design,school,31,13,6,8,4,12,5,16,434
+design,collaboration,31,7,6,7,13,12,11,7,482
+design,problem,31,6,5,6,10,8,10,12,513
+design,proposals,31,1,1,2,0,1,1,1,59
+design,mission,30,2,1,3,2,1,2,0,56
+design,perspective,29,7,6,2,3,7,11,7,430
+design,science,27,8,8,10,6,17,9,12,469
+design,philosophy,27,4,4,2,5,4,5,7,164`
+
+function parseData(str) {
+    let arr = str.split('\n');
+    let jsonObj = [];
+    let headers = arr[0].split(',');
+    for (let i = 1; i < arr.length; i++) {
+        let data = arr[i].split(',');
+        let obj = {};
+        for (let j = 0; j < data.length; j++) {
+            obj[headers[j].trim()] = data[j].trim();
+        }
+        jsonObj.push(obj);
+    }
+    return jsonObj
+}
+
+let uistData = parseData(uistCsv);
+let drsData = parseData(drsCsv);
+console.log(drsData)
+
+
+let width = 1500;
+let height = 1000;
+
+let svg = d3.select("body")
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("width", "90%")
+    .attr("height", "98%")
+    .classed("svg-content", true);
+
+let drsScale = d3.scaleLinear()
+    .domain([0, 500])
+    .range([1, 0.5]);
+
+let csScale = d3.scaleLinear()
+    .domain([0, 200])
+    .range([0, 0.5]);
+
+let yScale = d3.scaleLinear()
+    .domain([0, 2550])
+    .range([1, 0]);
+
+let xAxis = svg.append('line')
+    .style("stroke", "black")
+    .style("stroke-width", 2)
+    .attr("x1", 0)
+    .attr("y1", "96%")
+    .attr("x2", "100%")
+    .attr("y2", "96%");
+
+let dataText = svg.append("text")
+    .attr("fill", "black")
+    .attr("class", "searchWord")
+    .text("Design")
+    .attr("x", "15%")
+    .attr("y", "60%")
+
+let titleText = svg.append("text")
+    .attr("fill", "black")
+    .attr("class", "titleText")
+    .text("UIST 2021 x DRS 2020")
+    .attr("x", "0%")
+    .attr("y", "2%");
+
+function addDataNodes(data, color, scale) {
+    svg.selectAll(".svg-content")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => {
+        let sum = parseInt(d.nOne) + parseInt(d.nThree) + parseInt(d.nFive) + parseInt(d.nTen) + parseInt(d.nFifteen) + parseInt(d.nTwenty);
+        console.log(d.Word + " " + (drsScale(sum) * 100) + "%")
+        return (scale(sum) * 100) + "%"
+    })
+    .attr("cy", (d) => {
+        return (yScale(d.wordOccurrence) * 100) + "%";
+    })
+    .attr("r", 6)
+    .attr("fill", color)
+    .on("click", (d) => {
+        console.log(d.Word)
+    });
+}
+
+addDataNodes(drsData, "blue", drsScale);
+addDataNodes(uistData, "red", csScale)
+
+// let Drscircles = svg.selectAll(".svg-content")
+//     .data(drsData)
+//     .enter()
+//     .append("circle")
+//     .attr("cx", (d) => {
+//         let sum = parseInt(d.nOne) + parseInt(d.nThree) + parseInt(d.nFive) + parseInt(d.nTen) + parseInt(d.nFifteen) + parseInt(d.nTwenty);
+//         console.log(d.Word + " " + (drsScale(sum) * 100) + "%")
+//         return (drsScale(sum) * 100) + "%"
+//     })
+//     .attr("cy", (d) => {
+//         return (yScale(d.wordOccurrence) * 100) + "%";
+//     })
+//     .attr("r", 5)
+//     .attr("fill", "blue")
+//     .on("click", (d) => {
+//         console.log(d.Word)
+//     });
+
+// let Cscircles = svg.selectAll(".svg-content")
+//     .data(uistData)
+//     .enter()
+//     .append("circle")
+//     .attr("cx", (d) => {
+//         let sum = parseInt(d.nOne) + parseInt(d.nThree) + parseInt(d.nFive) + parseInt(d.nTen) + parseInt(d.nFifteen) + parseInt(d.nTwenty);
+//         console.log(d.Word + " " + sum)
+//         return (csScale(sum) * 100) + "%"
+//     })
+//     .attr("cy", (d) => {
+//         return (yScale(d.wordOccurrence) * 100) + "%"
+//     })
+//     .attr("r", 5)
+//     .attr("fill", "red");//"#8232df");
+
