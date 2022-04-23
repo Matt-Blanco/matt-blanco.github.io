10a00fdeef9294da0992e685cdba1809dcf0abac
Matt-Blanco <mattmblanco@gmail.com>
2022-04-23 17:04:39 -0400

    implemented basic react foundation

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import './style.css'
+
+function App() {
+  return (
+    <body>
+      <div id="MainHeader">
+        <div id="title">
+          <h1 id="hey">Hey I'm Matt</h1>
+        </div>
+        <div id="about">
+          <p id="aboutText">
+            I'm a designer and software engineer focusing on trying to make complex information easier for people to understand!
+            If any of this seems cool to you feel free to drop a line I'm always up for a chat!
+          </p>
+          <div id="QuickLinks">
+            <a class="link" id="email" href="mattmblanco@gmail.com"> email </a>
+            <a class="link" id="github" href="https://github.com/Matt-Blanco"> github </a>
+            <a class="link" id="linkedIn" href="https://www.linkedin.com/in/mattblanco/"> linkedIn </a>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
