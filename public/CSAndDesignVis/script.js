62b1c5068d877b5d6697fa5fc7a2c7a46adb94c2
Matt-Blanco <mattmblanco@gmail.com>
2022-04-24 16:32:57 -0400

    made projects clickable

---
+++ public/CSAndDesignVis/script.js
@@ -0,0 +1,366 @@
+let uistCsv = `Search_Term,Word,nOne,nThree,nFive,nTen,nFifteen,nTwenty,nTwentyFive,nFifty,wordOccurrence,conf
+design,space,99,0,2,3,1,1,1,0,544,uist
+design,tool,51,2,2,3,1,2,3,4,644,uist
+design,process,49,0,2,4,3,2,1,3,603,uist
+design,goals,27,0,1,0,1,2,0,0,77,uist
+design,guidelines,24,3,1,0,1,1,0,0,78,uist
+design,tools,24,2,0,3,6,3,4,1,670,uist
+design,parameters,22,2,1,0,1,2,2,1,416,uist
+design,drawing,20,0,0,0,1,1,2,1,189,uist
+design,considerations,16,1,0,0,0,0,0,0,44,uist
+design,implications,16,0,0,2,1,0,0,0,63,uist
+design,vibrotactile,12,4,2,4,4,5,1,3,231,uist
+design,principles,12,0,2,0,0,0,0,0,67,uist
+design,survey,12,0,0,0,2,0,1,0,206,uist
+design,system,9,10,9,9,7,7,2,9,1741,uist
+design,goal,9,1,3,0,0,0,0,0,192,uist
+design,applications,8,3,1,1,0,4,0,2,720,uist
+design,patterns,8,2,1,0,0,1,1,0,229,uist
+design,choices,8,0,0,0,0,0,0,0,37,uist
+design,elements,7,0,0,1,0,0,1,4,503,uist
+design,principle,6,0,1,1,0,0,0,0,72,uist
+design,study,5,13,7,10,7,5,6,4,1653,uist
+design,virtual,5,3,6,6,3,5,1,7,1539,uist
+design,based,5,2,1,3,1,1,5,1,910,uist
+design,material,5,1,3,0,2,1,0,1,422,uist
+design,session,5,0,0,1,0,2,1,0,168,uist
+design,heuristics,5,0,0,1,0,1,0,1,60,uist
+design,drawings,5,0,0,0,0,1,1,0,21,uist
+design,practice,5,0,0,0,0,0,0,2,153,uist
+design,iteration,5,0,0,0,0,0,0,0,33,uist
+design,decisions,5,0,0,0,0,0,0,0,41,uist
+design,criteria,5,0,0,0,0,0,0,0,48,uist
+design,example,4,8,2,0,1,2,2,3,1145,uist
+design,implementation,4,2,2,0,1,0,1,1,267,uist
+design,methods,4,2,1,1,1,2,1,0,536,uist
+design,approach,4,2,0,1,3,0,0,2,829,uist
+design,features,4,1,0,2,1,0,0,1,419,uist
+design,files,4,1,0,0,0,2,0,0,220,uist
+design,prototyping,4,0,2,2,1,0,3,1,212,uist
+design,intent,4,0,2,0,1,0,0,0,106,uist
+design,requirements,4,0,1,2,0,1,0,0,141,uist
+design,challenge,4,0,1,0,1,0,0,1,149,uist
+design,guideline,4,0,1,0,0,0,0,0,14,uist
+design,automation,4,0,0,1,0,0,1,0,142,uist
+design,thinking,4,0,0,0,1,0,0,1,26,uist
+design,morgan,4,0,0,0,0,0,0,0,22,uist
+design,users,3,6,7,5,6,7,5,2,2391,uist
+design,work,3,5,3,4,2,3,6,4,1375,uist
+design,techniques,3,2,4,1,2,1,0,1,462,uist
+design,insights,3,2,2,0,0,1,0,0,79,uist
+design,tactile,3,2,2,0,0,0,1,3,562,uist`
+
+let drsCsv = `Search_Term,Word,nOne,nThree,nFive,nTen,nFifteen,nTwenty,nTwentyFive,nFifty,wordOccurrence,conf
+design,process,506,42,35,37,45,45,32,34,2128,drs
+design,education,306,27,35,19,30,28,26,19,978,drs
+design,thinking,292,19,18,22,20,24,12,19,940,drs
+design,studies,204,48,22,25,22,16,23,16,784,drs
+design,practice,166,46,18,19,17,23,34,23,986,drs
+design,processes,103,15,10,14,11,11,13,15,625,drs
+design,methods,98,17,27,13,13,13,13,20,800,drs
+design,dna,92,4,4,6,0,6,3,0,123,drs
+design,issues,91,22,13,15,14,14,4,8,506,drs
+design,studio,83,4,8,8,11,7,9,9,357,drs
+design,management,74,18,7,15,11,14,18,16,568,drs
+design,practices,71,15,9,8,10,6,6,9,610,drs
+design,teams,71,3,8,6,7,8,2,7,342,drs
+design,innovation,70,66,23,30,22,27,31,25,872,drs
+design,approach,69,16,20,15,22,13,16,10,973,drs
+design,solutions,68,7,6,1,12,8,9,6,402,drs
+design,activities,67,10,7,9,10,3,8,4,660,drs
+design,students,61,27,18,21,22,21,29,18,1252,drs
+design,approaches,61,8,12,5,14,14,10,10,512,drs
+design,projects,60,9,10,6,16,14,14,8,540,drs
+design,concepts,60,4,19,13,13,13,10,9,445,drs
+design,project,56,13,17,11,20,18,19,16,1044,drs
+design,concept,56,7,8,5,4,13,5,8,459,drs
+design,researchers,54,15,4,1,1,5,5,4,352,drs
+design,tools,53,18,8,17,9,16,16,9,617,drs
+design,team,53,2,19,4,11,13,7,13,649,drs
+design,sprint,53,0,1,2,0,1,4,4,60,drs
+design,principles,52,4,5,5,10,7,5,7,340,drs
+design,sprints,52,1,2,0,0,1,1,0,52,drs
+design,practitioners,46,12,8,5,5,3,5,3,208,drs
+design,outcomes,45,5,4,11,4,3,8,4,426,drs
+design,heuristics,44,1,1,1,3,1,3,0,69,drs
+design,space,43,11,8,8,3,8,7,8,620,drs
+design,activity,42,5,7,2,5,5,4,6,433,drs
+design,history,41,5,5,4,6,5,4,4,234,drs
+design,strategy,38,11,5,8,9,5,7,7,312,drs
+design,theory,34,10,16,12,7,6,7,14,529,drs
+design,disciplines,33,2,2,3,8,5,4,3,249,drs
+design,work,32,17,9,19,18,20,12,14,1321,drs
+design,considerations,32,5,1,5,0,1,3,4,149,drs
+design,strategies,32,3,5,9,5,7,5,7,356,drs
+design,ideas,32,1,4,3,7,12,8,4,565,drs
+design,school,31,13,6,8,4,12,5,16,434,drs
+design,collaboration,31,7,6,7,13,12,11,7,482,drs
+design,problem,31,6,5,6,10,8,10,12,513,drs
+design,proposals,31,1,1,2,0,1,1,1,59,drs
+design,mission,30,2,1,3,2,1,2,0,56,drs
+design,perspective,29,7,6,2,3,7,11,7,430,drs
+design,science,27,8,8,10,6,17,9,12,469,drs
+design,philosophy,27,4,4,2,5,4,5,7,164,drs`
+
+function parseData(str) {
+    let arr = str.split('\n');
+    let jsonObj = [];
+    let headers = arr[0].split(',');
+    for (let i = 1; i < arr.length; i++) {
+        let data = arr[i].split(',');
+        let obj = {};
+        for (let j = 0; j < data.length; j++) {
+            obj[headers[j].trim()] = data[j].trim();
+        }
+        jsonObj.push(obj);
+    }
+    return jsonObj
+}
+
+function sumCoOccurrence(d, scale) {
+    let sum = parseInt(d.nOne) + parseInt(d.nThree) + parseInt(d.nFive) + parseInt(d.nTen) + parseInt(d.nFifteen) + parseInt(d.nTwenty);
+    return (scale(sum))
+}
+
+function combineData() {
+    let arr = uistCsv.split(`\n`).concat(drsCsv.split(`\n`).slice(1));
+    let jsonObj = [];
+    let baseHeaders = arr[0].split(',');
+    let sum = (d) => { return parseInt(d.nOne) + parseInt(d.nThree) + parseInt(d.nFive) + parseInt(d.nTen) + parseInt(d.nFifteen) + parseInt(d.nTwenty) };
+    for (let i = 1; i < arr.length; i++) {
+        let data = arr[i].split(',');
+        let obj = {};
+        for (let j = 0; j < data.length; j++) {
+            obj[baseHeaders[j].trim()] = data[j].trim();
+        }
+
+        let objToPush = {}
+        if (!jsonObj.includes(obj.Word)) {
+            objToPush = {
+                "word": obj.Word,
+                "coSum": sum(obj),
+                "conf": obj.conf === "drs" ? 1 : 0,
+                "frequency": obj.wordOccurrence
+            };
+        }
+        jsonObj.push(objToPush);
+    }
+    return jsonObj.filter((val, index, arr) => {
+        let opConf = val.conf === 0 ? 1 : 0;
+        return arr.some((el) => { return val.word === el.word && val.conf !== el.conf })
+    })
+}
+
+let dataSet1 = parseData(uistCsv);
+let dataSet2 = parseData(drsCsv);
+
+let WIDTH = 1400;
+let HEIGHT = 750;
+
+let svg = d3.select("body")
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("width", WIDTH)//"90%")
+    .attr("height", HEIGHT)//"98%")
+    .classed("svg-content", true);
+
+let rightScale = d3.scaleLinear()
+    .domain([0, 800])
+    .range([WIDTH, WIDTH / 2]);
+
+let leftScale = d3.scaleLinear()
+    .domain([0, 110])
+    .range([0, WIDTH / 2]);
+
+let yScale = d3.scaleLinear()
+    .domain([0, 2550])
+    .range([HEIGHT, 0]);
+
+xAxis = svg.append('line')
+    .style("stroke", "black")
+    .style("stroke-width", 2)
+    .attr("x1", 0)
+    .attr("y1", "3%")
+    .attr("x2", "100%")
+    .attr("y2", "3%");
+
+dataText = svg.append("text")
+    .attr("fill", "black")
+    .attr("class", "searchWord")
+    .text("Design")
+    .attr("x", "15%")
+    .attr("y", "60%")
+
+titleText = svg.append("text")
+    .attr("fill", "black")
+    .attr("class", "titleText")
+    .text("UIST 2021 x DRS 2020")
+    .attr("x", "0%")
+    .attr("y", "2%")
+    .attr("id", "title");
+
+csLegend = svg.append("circle")
+    .attr("fill", "red")
+    .attr("r", 6)
+    .attr("cx", "27%")
+    .attr("cy", "1.2%")
+
+csLegendTitle = svg.append("text")
+    .attr("fill", "black")
+    .text("CS Conference Data")
+    .attr("x", "28%")
+    .attr("y", "2%");
+
+designLegend = svg.append("circle")
+    .attr("fill", "blue")
+    .attr("r", 6)
+    .attr("cx", "40%")
+    .attr("cy", "1.2%")
+
+designLegendTitle = svg.append("text")
+    .attr("fill", "black")
+    .text("Design Journal Data")
+    .attr("x", "41%")
+    .attr("y", "2%");
+
+yAxisTitle = svg.append("text")
+    .attr("fill", "black")
+    .text("Word Frequency")
+    .attr("y", "2%")
+    .attr("x", "-30%")
+    .attr("transform", "rotate(-90)");
+
+xAxisTitle = svg.append("text")
+    .attr("fill", "black")
+    .text("Design Co-Occurrences")
+    .attr("y", "98%")
+    .attr("x", "42%");
+
+xAxisHelpText = svg.append("text")
+    .attr("fill", "black")
+    .text("(Closer to center means more co-occurrences)")
+    .attr("y", "100%")
+    .attr("x", "36%");
+
+svg.append("text")
+    .attr("fill", "black")
+    .text("0")
+    .attr("y", "99%")
+    .attr("x", "99%");
+
+svg.append("text")
+    .attr("fill", "black")
+    .text("0")
+    .attr("y", "99%")
+    .attr("x", "0%");
+
+let defs = svg.append("defs");
+let gradient = defs.append("linearGradient")
+    .attr("id", "svgGradient")
+    .attr("x1", "0%")
+    .attr("x2", "100%")
+    .attr("y1", "25%")
+    .attr("y2", "100%");
+gradient.append("stop")
+    .attr('class', 'start')
+    .attr("offset", "0%")
+    .attr("stop-color", "red")
+    .attr("stop-opacity", 1);
+gradient.append("stop")
+    .attr('class', 'end')
+    .attr("offset", "100%")
+    .attr("stop-color", "blue")
+    .attr("stop-opacity", 1);
+
+function addDataNodes(data, color, scale) {
+    svg.selectAll(".svg-content")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => {
+            return sumCoOccurrence(d, scale);
+        })
+        .attr("cy", (d) => {
+            return (yScale(d.wordOccurrence))
+        })
+        .attr("r", 6)
+        .attr("fill", color)
+        .attr("class", (d) => { return `${d.Word}Node dataNode ${color}Node` })
+        .on("mouseover", (d) => {
+            svg.selectAll(`.${d.Word}Text`).attr("opacity", 1)
+            svg.selectAll(`.dataNode`).attr("opacity", 0.25)
+            svg.selectAll(`.dataNode`).attr("opacity", 0.25)
+            svg.selectAll(`.${d.Word}Node`).attr("opacity", 1)
+            svg.selectAll(`.bezierLine`).attr("opacity", 0.25)
+            svg.select(`.${d.Word}Line`).attr("opacity", 1)
+        })
+        .on("mouseout", (d) => {
+            svg.selectAll(`.${d.Word}Text`).attr("opacity", 0)
+            svg.selectAll(`.dataNode`).attr("opacity", 1)
+            svg.selectAll(`.bezierLine`).attr("opacity", 0.5)
+        });
+
+
+    for (let i = 0; i < 3; i++) {
+        svg.selectAll(".svg-content")
+        .data(data)
+        .enter()
+        .append("text")
+        .text((d) => {
+            let coOccurrenceSum = parseInt(d.nOne) + parseInt(d.nThree) + parseInt(d.nFive) + parseInt(d.nTen) + parseInt(d.nFifteen) + parseInt(d.nTwenty)
+            let textLines = [`Co-Occurrences - ${coOccurrenceSum}`, `Frequency - ${d.wordOccurrence}`, `${d.Word}:`]
+            return textLines[i]
+        })
+        .attr("x", (d) => {
+            return (sumCoOccurrence(d, scale) - 40);
+        })
+        .attr("y", (d) => {
+            return (yScale(d.wordOccurrence) - (10 + (i * 14)))
+        })
+        .attr("opacity", 0)
+        .attr("class", (d) => { return `${d.Word}Text`})
+    }
+}
+
+function drawLines() {
+    let commonWords = dataSet1.filter((el) => { return dataSet2.some(data => { return (data.Word === el.Word) }) }).map(el => { return el.Word })
+
+    function getPoints(word) {
+        let startData = dataSet1.find(el => { return el.Word === word });
+        let endData = dataSet2.find(el => { return el.Word === word });
+        let startX = sumCoOccurrence(startData, leftScale);
+        let startY = yScale(startData.wordOccurrence);
+        let endX = sumCoOccurrence(endData, rightScale);
+        let endY = yScale(endData.wordOccurrence);
+        let y2 = yScale(startData.wordOccurrence) + 50 > HEIGHT ? yScale(startData.wordOccurrence) - 50 : yScale(startData.wordOccurrence) + 50;
+        let y3 = yScale(endData.wordOccurrence) + 50 > HEIGHT ? yScale(endData.wordOccurrence) + (HEIGHT - yScale(endData.wordOccurrence)) : yScale(endData.wordOccurrence) - 50;
+
+        return [[startX, startY], [startX + 200, y2], [endX - 200, y3], [endX, endY]]
+    }
+
+    let bezierLine = d3.line()
+        .x(function (d) {
+            return d[0];
+        })
+        .y(function (d) {
+            return d[1];
+        })
+        .curve(d3.curveBasis);
+
+    svg.selectAll(".svg-content")
+        .data(commonWords)
+        .enter()
+        .append('path')
+        .attr("d", (d) => {
+            return bezierLine(getPoints(d))
+        })
+        .attr("class", (d) => { return `${d}Line bezierLine` })
+        .attr("stroke", "url(#svgGradient)")
+        .attr("stroke-WIDTH", 2)
+        .attr("fill", "none")
+        .attr("opacity", 0.5);
+}
+
+drawLines();
+addDataNodes(dataSet2, "blue", rightScale);
+addDataNodes(dataSet1, "red", leftScale);
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,7 +5,6 @@ import designVisImg from "./data/ProjectImages/designAcrossTime.JPG"
 import excelsiorImg from "./data/ProjectImages/Excelsior2022Cover.png"
 import filmIng from "./data/ProjectImages/film.JPG"
 import posterImg from "./data/ProjectImages/hopper.png"
-
 import Background from "./Background"
 
 
@@ -15,31 +14,37 @@ export default function Projects() {
         title: "Accessible Visualization Design",
         content: "In collaboration with people with visual disabilities, we explore textual and screen reader modalities for representing data.",
         imgUrl: accessImg,
+        clickUrl: "http://vis.csail.mit.edu/"
     },
     {
         title: "Design Across Disciplines",
         content:"A visualization comparing how design language is used in different settings from a computer science academic conference to a design research jorunal.",
-        imgUrl: designVisImg
+        imgUrl: designVisImg,
+        clickUrl: `${process.env.PUBLIC_URL}CSAndDesignVis/index.html`
     },
     {
         title: "Project Atlas",
         content: "A Visual Studio Code Extension that aims to visualize Java codebases to increase the learnability of code.",
-        imgUrl: atlastImg
+        imgUrl: atlastImg,
+        clickUrl: "https://github.com/Solome6/Project-Atlas"
     },
     {
         title: "Excelsior 2022",
         content: "Designed the cover and layout of Excelsior. The 2022 installation of Northeastern University Outing Club's premiere magazine recapping stories throughout the past two semesters.",
-        imgUrl: excelsiorImg
+        imgUrl: excelsiorImg,
+        clickUrl: excelsiorImg
     },
     {
         title: "Poster a Day (2021)",
         content: "Practicing graphic design skills by creating a unique poster evey day and posting it on my social media.",
-        imgUrl: posterImg
+        imgUrl: posterImg,
+        clickUrl: "https://photos.app.goo.gl/62cRfi9oX3rG4dWA7"
     },
     {
         title: "Film Photography",
         content: "A Collection of all the film photos I've taken to hopefully as I've learned more about the craft.",
-        imgUrl: filmIng
+        imgUrl: filmIng,
+        clickUrl: "https://photos.app.goo.gl/pqjHCK5TH1LWqfwo6"
     }]
 
 
@@ -52,7 +57,8 @@ export default function Projects() {
                     cards.map((card) => (
                         <Card title={card.title}
                             content={card.content}
-                            imgUrl={card.imgUrl} />
+                            imgUrl={card.imgUrl}
+                            clickUrl={card.clickUrl} />
                     ))
                 }
             </div>
@@ -62,8 +68,13 @@ export default function Projects() {
 }
 
 function Card(props) {
+
+    const onClickFunc = () => {
+        window.open(props.clickUrl, '_blank');
+    }
+
     return (
-        <div className="card">
+        <div onClick={onClickFunc} className="card">
             <img src={props.imgUrl}
                 alt={props.alt || 'Image'} />
             <div className="card-content">
