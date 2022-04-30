f05f53cf15fc649cc2e2c44660a8e7b4ce447bec
Matt-Blanco <mattmblanco@gmail.com>
2022-04-29 22:48:14 -0400

    made circle animation interactive

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Sketch from "react-p5";
 
-let width = window.screen.availWidth;
-let height = window.screen.availHeight;
+let width = window.innerWidth;
+let height = window.innerHeight;
 console.log(`${width} ${height}`)
 let circles = [{
     x: width / 2,
@@ -13,7 +13,8 @@ let circles = [{
     strokeWeight: 9,
     width: 300,
     height: 300,
-    refreshRate: 60
+    refreshRate: 60,
+    mouseRatio: 0.50
 },
 {
     x: width / 2,
@@ -24,7 +25,8 @@ let circles = [{
     strokeWeight: 5,
     width: 400,
     height: 400,
-    refreshRate: 30
+    refreshRate: 30,
+    mouseRatio: 0.75
 },
 {
     x: width / 2,
@@ -35,7 +37,8 @@ let circles = [{
     strokeWeight: 22,
     width: 150,
     height: 150,
-    refreshRate: 120
+    refreshRate: 120,
+    mouseRatio: 0.25
 },
 {
     x: width / 2,
@@ -46,7 +49,8 @@ let circles = [{
     strokeWeight: 2,
     width: 500,
     height: 500,
-    refreshRate: 30
+    refreshRate: 30,
+    mouseRatio: 1
 }, {
     x: width / 2,
     y: height / 2,
@@ -56,7 +60,8 @@ let circles = [{
     strokeWeight: 25,
     width: 50,
     height: 50,
-    refreshRate: 300
+    refreshRate: 300,
+    mouseRatio: 0.10
 }];
 const bg2 = "White";
 let refreshCount
@@ -72,41 +77,54 @@ export default function Background(props) {
     };
 
     const draw = (p5) => {
+        // console.log(`${p5.mouseX} - ${p5.mouseY}`)
 
-
+        p5.background(bg2);
         if (width !== p5.windowWidth || height !== p5.windowHeight) {
             width = p5.windowWidth;
             height = p5.windowHeight;
             p5.resizeCanvas(width, height);
         }
 
+        if (p5.windowWidth <= 600) {
+            circles.forEach(circle => {
+                if (refreshCount % circle.refreshRate === 0) {
+                    circle.stepX = p5.random(25, 75) / 60 * p5.random([-1, 1]);
+                    circle.stepY = p5.random(25, 75) / 60 * p5.random([-1, 1]);
+                }
+            })
+    
+    
+            circles.forEach(circle => {
+                if (circle.stepX + circle.x < 0 || circle.stepX + circle.x > width) {
+                    circle.stepX *= -1
+                }
+                if (circle.stepY + circle.y < 0 || circle.stepY + circle.y > height) {
+                    circle.stepY *= -1
+                }
+                p5.strokeWeight(circle.strokeWeight)
+                p5.stroke(circle.color)
+                circle.x += circle.stepX
+                circle.y += circle.stepY
+                p5.ellipse(circle.x, circle.y, circle.width, circle.height)
+            })
+    
+            p5.colorMode(p5.RGB)
+    
+            refreshCount += 1
+        } else {
+            circles.forEach(circle => {
 
-        p5.background(bg2);
-        circles.forEach(circle => {
-            if (refreshCount % circle.refreshRate === 0) {
-                circle.stepX = p5.random(25, 75) / 60 * p5.random([-1, 1]);
-                circle.stepY = p5.random(25, 75) / 60 * p5.random([-1, 1]);
-            }
-        })
+                circle.x = p5.mouseX > width/2 ? width/2 + ((p5.mouseX - width/2) * circle.mouseRatio) :
+                width/2 - ((width/2 - p5.mouseX) * circle.mouseRatio);
+                circle.y = p5.mouseY > height/2 ? height/2 + (p5.mouseY - height/2) * circle.mouseRatio :
+                height/2 - (height/2 - p5.mouseY) * circle.mouseRatio;
 
-
-        circles.forEach(circle => {
-            if (circle.stepX + circle.x < 0 || circle.stepX + circle.x > width) {
-                circle.stepX *= -1
-            }
-            if (circle.stepY + circle.y < 0 || circle.stepY + circle.y > height) {
-                circle.stepY *= -1
-            }
-            p5.strokeWeight(circle.strokeWeight)
-            p5.stroke(circle.color)
-            circle.x += circle.stepX
-            circle.y += circle.stepY
-            p5.ellipse(circle.x, circle.y, circle.width, circle.height)
-        })
-
-        p5.colorMode(p5.RGB)
-
-        refreshCount += 1
+                p5.strokeWeight(circle.strokeWeight)
+                p5.stroke(circle.color)
+                p5.ellipse(circle.x, circle.y, circle.width, circle.height)
+            })
+        }
     };
     return <Sketch setup={setup} draw={draw} />;
 };
